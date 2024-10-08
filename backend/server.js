dbad41b2f55ea72b0e5079fe7670c2b5133f1f49
Khushbu Raju Burde <khushbub@cybage.com>
2024-10-08 11:38:23 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const k8s = require('@kubernetes/client-node');
 const os = require('os');
-
 const app = express();
 const port = 5000;
 
@@ -29,7 +28,7 @@ app.get('/api/data', async (req, res) => {
         const podIp = req.connection.remoteAddress;
         const containerId = os.hostname();
         const nodeIp = os.networkInterfaces()['eth0'][0].address; // Assumes eth0 is used
-        const region = process.env.AWS_REGION || 'us-west-2'; // Specify region
+        const region = process.env.AWS_REGION || 'us-east-1'; // Specify region
 
         // Save data to RDS
         const query = 'INSERT INTO pod_info (pod_ip, container_id, node_ip, account_no, region) VALUES (?, ?, ?, ?, ?)';
